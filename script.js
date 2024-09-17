543a3c585e4dfd58b2dfbb411766c8db984a66f9
Daniel Andrade <andradedev7@gmail.com>
2024-09-17 06:28:05 -0300

    troquei esle's pelo operador ||

--- script.js
+++ script.js
@@ -2,23 +2,17 @@
 function playRockPaperScissor(player1, player2){
 let resultado = `${player1, player2}`
 
-if(player1=="Pedra" && player2=="Pedra"){
+if ((player1=="Pedra" && player2=="Pedra") || 
+(player1 =="Papel" && player2 == "Papel") ||
+(player1 =="Tesoura" && player2 == "Tesoura")){
     resultado = "Empate!";
-} else if( player1 =="Papel" && player2 == "Papel"){
-    resultado = "Empate!";
-} else if( player1 =="Tesoura" && player2 == "Tesoura"){
-    resultado = "Empate!";
-} else if( player1 =="Papel" && player2 == "Pedra"){
+}if(( player1 =="Papel" && player2 == "Pedra")||
+ (player1 =="Tesoura" && player2 == "Papel")||
+ (player1 =="Pedra" && player2 == "Tesoura")){
     resultado = "Jogador 1 venceu!";
-} else if( player1 =="Tesoura" && player2 == "Papel"){
-    resultado = "Jogador 1 venceu!";
-} else if( player1 =="Pedra" && player2 == "Tesoura"){
-    resultado = "Jogador 1 venceu!";
-} else if( player1 =="Papel" && player2 == "Tesoura"){
-    resultado = "Jogador 2 venceu!";
-} else if( player1 =="Pedra" && player2 == "Papel"){
-    resultado = "Jogador 2 venceu!";
-} else if( player1 =="Tesoura" && player2 == "Pedra"){
+}if(( player1 =="Papel" && player2 == "Tesoura")||
+ (player1 =="Pedra" && player2 == "Papel")||
+ (player1 =="Tesoura" && player2 == "Pedra")){
     resultado = "Jogador 2 venceu!";
 } 
 
